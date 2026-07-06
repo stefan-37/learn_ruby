386139550bfa1125e331f259e44c868257eb3f0f
stefan-37 <stefans.solomon@ulbsibiu.ro>
2026-07-06 14:38:53 +0300

    09_timer

---
+++ 09_timer/timer.rb
@@ -0,0 +1,25 @@
+class Timer
+
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds
+    @seconds
+  end
+
+  def time_string
+    hours = 0
+    minutes = 0
+    seconds = 0
+
+    hours = @seconds / 3600
+    minutes = (@seconds % 3600) / 60
+    seconds = @seconds % 60
+
+    format("%02d:%02d:%02d", hours, minutes, seconds)
+  end
+
+end
