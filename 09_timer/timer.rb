e1156711f822873684d46dcf2a761f7bbcbc407a
stefan-37 <stefans.solomon@ulbsibiu.ro>
2026-07-06 18:45:34 +0300

    add guard for negative values and test for it

--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -1,20 +1,28 @@
 class Timer
 
-  attr_accessor :seconds
+  attr_reader :seconds
+
+  SECONDS_PER_MINUTE = 60
+  MINUTES_PER_HOUR = 60
+  SECONDS_PER_HOUR = SECONDS_PER_MINUTE * MINUTES_PER_HOUR
 
   def initialize
     @seconds = 0
   end
 
-  def seconds
-    @seconds
+  def seconds=(seconds)
+    if seconds < 0
+      puts "Invalid number of seconds. The timer can't be set to #{seconds} seconds"
+      return
+    end
+    @seconds = seconds
   end
 
   def time_string
     
-    hours = @seconds / 3600
-    minutes = (@seconds % 3600) / 60
-    seconds = @seconds % 60
+    hours = @seconds / SECONDS_PER_HOUR
+    minutes = (@seconds % SECONDS_PER_HOUR) / SECONDS_PER_MINUTE
+    seconds = @seconds % SECONDS_PER_MINUTE
 
     format("%02d:%02d:%02d", hours, minutes, seconds)
   end
